d0c8b49bfd55e852861ab2cfd8504b626f058572
suheerthedev <khansuheer1@gmail.com>
2024-10-23 07:25:33 +0500

    Practiced Shared Pref by making counter app

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences_practice/base_class.dart';
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,8 +10,53 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+  LocalStorage.initialize().then((_){
+    setState(() {
+      counter = LocalStorage.getInt('counter') ?? 0;
+    });
+  });
+  }
+
+  int counter = 0;
+
+  void _incrementCounter(){
+    setState(() {
+      counter++;
+      LocalStorage.setInt('counter', counter);
+    });
+  }
+
+  void _decrementCounter(){
+    setState(() {
+      counter--;
+      LocalStorage.setInt('counter',counter);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return  Scaffold(
+      body: SafeArea(child: Center(
+        child: Text('$counter',style: const TextStyle(fontSize: 36, fontWeight: FontWeight.bold),),
+      )),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: FloatingActionButton(onPressed: _decrementCounter,child: const Icon(Icons.remove),),
+          ),
+        
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            child: FloatingActionButton(onPressed: _incrementCounter,child: const Icon(Icons.add),),
+          )
+        ],
+      ),
+    );
   }
 }
